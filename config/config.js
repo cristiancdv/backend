f9a1c9f6432ef7cc66da0d2c1cf43767973fe7ee
cristian <cristiancdv@gmail.com>
2021-12-29 17:10:59 -0300

    commit on master

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 const config = {
     appConfig: {
         host: process.env.APP_HOST,
-        port: process.env.APP_PORT
+        port: process.env.PORT
     },
     dbConfig: {
         host: process.env.DB_HOST,
